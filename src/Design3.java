bc656e94ae7ddea8549426582fbd10142b60225f
meet <panchalmeetr97@gmail.com>
2023-08-31 16:57:13 +0530

    31/8/23

---
+++ src/Design3.java
@@ -0,0 +1,13 @@
+public class Design3 {
+    public static void design3(String[]args){
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*                                     *  *");
+        System.out.println("*                                  *        *");
+        System.out.println("*        *  *                   *");
+        System.out.println("*      *       *              *");
+        System.out.println("*    *            *        *");
+        System.out.println("*  *                 *  *");
+        System.out.println("*");
+    }
+}
