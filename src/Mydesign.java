e742f10b0f523ca7de628926adf5b9120f2c64ca
meet <panchalmeetr97@gmail.com>
2023-08-31 17:04:04 +0530

    31/8/23

---
+++ src/Mydesign.java
@@ -0,0 +1,15 @@
+public class Mydesign {
+    public static void mydesign(String[]args) {
+        System.out.println(" - - - ");
+        System.out.println("|      |");
+        System.out.println("j      |");
+        System.out.println("a      |");
+        System.out.println("y      |");
+        System.out.println("d      |");
+        System.out.println("e      |");
+        System.out.println("e      |");
+        System.out.println("p      |");
+        System.out.println("|      |");
+        System.out.println(" - - - ");
+    }
+}
