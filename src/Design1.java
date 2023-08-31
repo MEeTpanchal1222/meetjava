3038b16f7afc93c49f060b74797d763e537152b0
meet <panchalmeetr97@gmail.com>
2023-08-31 16:41:40 +0530

    31/8/23

---
+++ src/Design1.java
@@ -0,0 +1,11 @@
+public class Design1 {
+    public static void design1(String[]args){
+        System.out.println("- - - - - - - -");
+        System.out.println("|             |");
+        System.out.println("|             |");
+        System.out.println("|   m e e t   |");
+        System.out.println("|             |");
+        System.out.println("|             |");
+        System.out.println("- - - - - - - -");
+    }
+}
