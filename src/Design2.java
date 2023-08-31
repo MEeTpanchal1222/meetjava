894b6c351b1efd3dc5c3a0a6ea916bd97ec4a761
meet <panchalmeetr97@gmail.com>
2023-08-31 16:51:28 +0530

    31/8/23

---
+++ src/Design2.java
@@ -0,0 +1,11 @@
+public class Design2{
+    public static void Design2(String[]args){
+        System.out.println("*");
+        System.out.println("*  *");
+        System.out.println("*  *  *");
+        System.out.println("*  *");
+        System.out.println("*");
+
+    }
+
+}
