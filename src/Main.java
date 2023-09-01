240bf2d25a36ed3938a8565b22157ba83ee13cfc
MEeTpanchal1222 <143181413+MEeTpanchal1222@users.noreply.github.com>
2023-09-01 00:31:24 -0700

    1/9/23

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!"+
+                "  this is my biodata");
+        System.out.println("                BIODATA           ");
+        System.out.println("    ");
+        System.out.println("Name:                            Meet R panchal");
+        System.out.println("  ");
+        System.out.println("Education Qualification:         12th pass");
+        System.out.println("  ");
+        System.out.println("Date of Birth:                   december 22, 2005");
+        System.out.println("  ");
+        System.out.println("Language Known:                  gujrati,hindi,English");
+        System.out.println("  ");
+        System.out.println("Correspondence Address:          xxxx nnn xxxxxx");
+        System.out.println("  ");
+        System.out.println("Cell No.:                        9426804903");
+        System.out.println("  ");
+        System.out.println("Email ID:                        panchalmeetr97@gmail.com");
+        System.out.println("  ");
+        System.out.println("Permanent Address:               xxxx nnn xxxxx");
+        System.out.println("  ");
+        System.out.println("Personal Handicap:               N/A");
+        System.out.println("Gender:                          Male");
+        System.out.println("  ");
+        System.out.println("Mariatal Status:                 Single");
+        System.out.println("  ");
+        System.out.println("Nationality:                     indian");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
+
