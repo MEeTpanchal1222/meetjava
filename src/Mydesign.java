56a223e2fe1c8fd1534aa99e50de77c94e34a500
MEeTpanchal1222 <143181413+MEeTpanchal1222@users.noreply.github.com>
2023-09-01 22:46:34 -0700

    Mydesign.java

--- src/Mydesign.java
+++ src/Mydesign.java
@@ -2,13 +2,13 @@ public class Mydesign {
     public static void mydesign(String[]args) {
         System.out.println(" - - - ");
         System.out.println("|      |");
-        System.out.println("j      |");
-        System.out.println("a      |");
-        System.out.println("y      |");
-        System.out.println("d      |");
+        System.out.println("|      |");
+        System.out.println("m      |");
         System.out.println("e      |");
         System.out.println("e      |");
-        System.out.println("p      |");
+        System.out.println("t      |");
+        System.out.println("|      |");
+        System.out.println("|      |");
         System.out.println("|      |");
         System.out.println(" - - - ");
     }
