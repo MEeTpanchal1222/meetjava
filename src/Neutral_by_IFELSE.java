44cb983e9c704461aa16665f88c5310f1f9af661
meetR panchal <panchalmeetr97@gmail.com>
2023-09-12 11:04:38 +0530

    12/9/23

---
+++ src/Neutral_by_IFELSE.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Neutral_by_IFELSE
+{
+public static void main(String[]args)
+        {
+        Scanner input = new Scanner(System.in);
+        byte a;
+        byte b=0;
+
+        System.out.print("Enter the number A:");
+        a = input.nextByte();
+
+        if(a<b)
+        {
+        System.out.println("A is negetive");
+        System.out.println(a);
+
+        }else if(a>b)
+        {
+        System.out.println("A is povsitev");
+        System.out.println(a);
+
+        }else
+        {
+            System.out.println(a);
+            System.out.println("An is neutral");
+        }
+
+        }
+        }
